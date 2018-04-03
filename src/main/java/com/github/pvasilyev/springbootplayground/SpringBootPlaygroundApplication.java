83bdd240b5ba8ab2463ecaea58a9358c2ae09d4f
pvasilyev <p_vasiliev@mail.ru>
2018-04-03 10:28:13 +0300

    Initial commit :: just put project files once they are downloaded from spring initialzr.

---
+++ src/main/java/com/github/pvasilyev/springbootplayground/SpringBootPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.github.pvasilyev.springbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+	}
+}
